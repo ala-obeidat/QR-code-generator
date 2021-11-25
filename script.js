c8fcac30931df951f984fef6226c53a461401c37
Ala Obeidat <ala_work@hotmail.com>
2021-11-25 21:12:41 +0300

    remove nodeJS and add query to code

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 $(document).ready(function(){
 $('#download').hide();
+if(location.href.indexOf('?code=')>-1)
+{
+	var code=location.href.split('?code=')[1];
+	$('#code-generate').val(code);
+	$('#generateBtn').click();
+}
+	});
 	$('#generateBtn').click(function(){
-	$('#result').html('');
-	var codeHtml= getGenerateQRcode($('#code-generate').val(), $('#code-width').val(), $('#code-hight').val(), 1,$('#code-border').prop('checked'));
-	$('#result').html(codeHtml);
-	$('#download').show();
-	});
-
-	$('#download').click(
+		$('#result').html('');
+		var codeHtml= getGenerateQRcode($('#code-generate').val(), $('#code-width').val(), $('#code-hight').val(), 1,$('#code-border').prop('checked'));
+		$('#result').html(codeHtml);
+		$('#download').show();
+		});
 	
-	function(){
-	var fileName=prompt("Enter file name : ","QR-Code");
-	var a = document.createElement('a');
-a.href = $('#result div img').attr("src");
-a.download = fileName+".png";
-document.body.appendChild(a);
-a.click();
-document.body.removeChild(a);
-	}
-	);
-	});
+		$('#download').click(
+		
+		function(){
+		var fileName=prompt("Enter file name : ","QR-Code");
+		var a = document.createElement('a');
+	a.href = $('#result div img').attr("src");
+	a.download = fileName+".png";
+	document.body.appendChild(a);
+	a.click();
+	document.body.removeChild(a);
+		}
+		);
